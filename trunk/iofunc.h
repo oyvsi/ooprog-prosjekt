4157c2c2dccae74b9247b94465bd840e3be4ec06
eirik.skogstad <eirik.skogstad@f2b5e8df-77c0-03c4-f9aa-19f8674c883b>
2011-03-28 10:28:31 +0000

    meir tab-trøbbel fiksa

--- trunk/iofunc.h
+++ trunk/iofunc.h
@@ -7,14 +7,14 @@
 class IOfunc {
 	private:
 		bool validate(char* txt, val_type v);
-		bool isalex(char c);                    //  Sjekker om c er æøåÆØÅ
+		bool isalex(char c);									//  Sjekker om c er æøåÆØÅ
 	public:
-		bool  is_number(char* c);               //  Sjekker om *c er et tall
-		char  to_upper(char up);                //  Returnerer uppercase
-		char* read_string(std::istream* in, char delim = '\n');
-		char* read_valid(char* txt, val_type v);
-		char* store_string(char* txt);
-		int	  read_number(char* txt, int min, int max);
+		bool	is_number(char* c);							//  Sjekker om *c er et tall
+		char	to_upper(char up);							//  Returnerer uppercase
+		char*	read_string(std::istream* in, char delim = '\n');
+		char*	read_valid(char* txt, val_type v);
+		char*	store_string(char* txt);
+		int		read_number(char* txt, int min, int max);
 };
 
 #endif
