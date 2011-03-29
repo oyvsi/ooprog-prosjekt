7d34f09878ae2ae73b47faf6965877b07e645219
eigilo <eigilo@gmail.com>
2011-03-29 12:29:41 +0000

    Løser verdensproblem...

--- global.h
+++ global.h
@@ -11,20 +11,12 @@ const int MAXGOALS		=  50;	//Maximum goals in a match
 const int DATELEN			=   9;	//Default length on date strings
 
 
-#ifdef _WIN32									//  Bokstavkoder for ∆ÿ≈ (windows):
+//  Bokstavkoder for ∆ÿ≈ (windows):
 const int UNI_ae = -111;
 const int UNI_AE = -110;
 const int UNI_oe = -101;
 const int UNI_OE =  -99;
 const int UNI_aa = -122;
 const int UNI_AA = -113;
-#else													//	Kanskje dette fungerer for unix:
-const int UNI_ae =  -61;
-const int UNI_AE =  -61;
-const int UNI_oe =  -61;
-const int UNI_OE =  -61;
-const int UNI_aa =  -61;
-const int UNI_AA =  -61;
-#endif
 
 #endif
